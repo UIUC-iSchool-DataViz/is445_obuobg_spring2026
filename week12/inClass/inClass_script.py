0294ccc15c7ca165763f02b5a7c7a97da6bc3f50
jnaiman <jnaiman.illinois.edu>
2026-04-08 18:22:52 -0500

    Auto-commit: Wed Apr  8 18:22:52 CDT 2026

--- week12/inClass/inClass_script.py
+++ week12/inClass/inClass_script.py
@@ -77,3 +77,8 @@ matplotlib
 """)
 
 st.subheader('Quick example widget')
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+  st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
