3b43c1a1b23fd5bbe73088b128faacf08acc8fe8
jnaiman <jnaiman.illinois.edu>
2026-04-08 18:24:09 -0500

    Auto-commit: Wed Apr  8 18:24:09 CDT 2026

--- week12/inClass/inClass_script.py
+++ week12/inClass/inClass_script.py
@@ -82,3 +82,5 @@ sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
   st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+
+st.write("""I'll update this example widget:""")
