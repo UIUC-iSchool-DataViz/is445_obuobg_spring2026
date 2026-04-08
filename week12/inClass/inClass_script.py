499a7aaaed116dcd7f0a14319a91ec461dd4e886
jnaiman <jnaiman.illinois.edu>
2026-04-08 18:29:21 -0500

    Auto-commit: Wed Apr  8 18:29:21 CDT 2026

--- week12/inClass/inClass_script.py
+++ week12/inClass/inClass_script.py
@@ -87,3 +87,10 @@ st.write("""I'll update this example widget:""")
 
 sentiment_mapping2 = ["one", "two", "three", "four", "five"]
 selected2 = st.feedback("stars", key="feedback2") # for this version, need to specify separate keys
+if selected2 is not None: # only run what is below if star is selected
+  if selected2 < 1:
+    st.markdown("Sorry you didn't want more stars :(")
+  elif selected2 < 3:
+    st.markdown("Glad you have chosen more stars")
+  else:
+    st.markdown("Hurray!  You like stars!")
