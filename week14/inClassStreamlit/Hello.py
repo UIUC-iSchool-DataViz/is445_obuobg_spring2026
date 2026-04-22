fd418267e3967eb4b3c2e928106cec0ad4d8c30a
jnaiman <jnaiman.illinois.edu>
2026-04-22 18:04:50 -0500

    Auto-commit: Wed Apr 22 18:04:50 CDT 2026

--- week14/inClassStreamlit/Hello.py
+++ week14/inClassStreamlit/Hello.py
@@ -1,6 +1,13 @@
 # we will often times use an app.py instead of a workbook so here is just a placeholder for that kind of file as well!
 import streamlit as st
 
+st.set_page_config(
+  page_title="Hello",
+  page_icon="👋"
+)
+
+st.sidebar.success("Select a Page")
+
 st.title('This is my fancy app!')
 
 st.header("This is a header")
